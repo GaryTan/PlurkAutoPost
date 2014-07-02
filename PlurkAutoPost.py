5fdb230841deb80c01db860d7046a505d98558ab
GaryTan <garytan@nisra.net>
2014-07-02 20:55:39 +0800

    Change to correct version

--- PlurkAutoPost.py
+++ PlurkAutoPost.py
@@ -3,6 +3,7 @@
 import urllib2, urllib
 import time
 import getpass
+import re
 from datetime import datetime
 
 print "Karma up up up !"
@@ -19,18 +20,22 @@ print "(format: HH:MM:SS)"
 time = raw_input("Time: ")
 print "\r"
 
+res = urllib2.urlopen('http://www.plurk.com/'+user).read()
+re = re.findall('"user_id": (\w+)', res)
+
 while 1:
 	timenow = str(datetime.now())[11:19]
 	if time == timenow:
 		cookies = urllib2.HTTPCookieProcessor()
 		opener = urllib2.build_opener(cookies)
+
 		data = {  "nick_name": user, "password": passwd, "login_token" : "3b1e3413a45932936432354ab3c062f8@n6t3bj", "logintoken" : "1"}
 		request = urllib2.Request(
-		        url     = 'http://www.plurk.com/Users/login',
-		        data    = urllib.urlencode(data))
+			url     = 'http://www.plurk.com/Users/login',
+			data    = urllib.urlencode(data))
 		f = opener.open(request)
 
-		data = {  "qualifier": "says", "content": post, "lang" : "tr_ch", "no_comments" : "0", "uid" : "9064266"}
+		data = {  "qualifier": "says", "content": post, "lang" : "tr_ch", "no_comments" : "0", "uid" : re[0]}
 		request = urllib2.Request(
 		        url     = 'http://www.plurk.com/TimeLine/addPlurk',
 		        data    = urllib.urlencode(data))
